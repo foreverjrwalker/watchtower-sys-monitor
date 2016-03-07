5dffc6dbc7258ea352a90f68b0f754f9a7f939a0
slaugms <slaugms@pindrop-gsd>
2016-03-06 20:57:54 -0600

    Began the integration of the server and the system watcher. Need to complete the following: -Updating the database after each iteration -Outputting JSON -Completing the translation of frontend commands to backend database calls -General Clean-up and refactoring

--- run_watchtower.sh
+++ run_watchtower.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/sh
 python manage.py runserver
+firefox 127.0.0.1:8000/watchtower/api?
--- sys_watcher.py
+++ sys_watcher.py
@@ -17,9 +17,8 @@ import inspect  # Debug Information
 import pprint
 import sqlite3
 import sys
-
 from time import sleep
-import time
+from time import time
 
 import psutil as PS
 
@@ -44,25 +43,24 @@ def fill_arguments(command, argumentList):
     return command
 
 def get_timestamp():
-    return "[" + str(datetime.datetime.now()) + "]"
-
-def get_short_timestamp():
     return  datetime.datetime.now().strftime("%b-%d %Y %H:%M:%S")
-
 
 def start_monitoring():
     # Start the Network Thread
     start_network_thread()
-    
-    while (EXECUTING):
-        # Update the Messsage
-        pprint.pprint(retrieve_current_system_state())
-        sleep(UPDATE_RATE)
+    # Connect to database
+    conn = connect_to_database("sysinfo.db.sqlite3")
 
+    while (EXECUTING):
+        # Update the message
+        current_state = retrieve_current_system_state()
+        pprint.pprint(current_state)
+        update_database(current_state, conn)
+        sleep(UPDATE_RATE)
     return True
 
 def start_network_thread():
-    # Launch Network speed thread
+    # Launch Network speed calculation daemon
     mythread = threading.Thread(target=calculate_network_speed,
                                 args=(current_network_throughput,))
     mythread.daemon = True
@@ -77,8 +75,31 @@ def writeout(message, error=False):
 
 def connect_to_database(file_location):
     # Connecting to the Database File
-    # sqlite3.connect(file_location)
-    pass
+    writeout("Creating Database: %s" % file_location)
+    conn = sqlite3.connect('file_location')
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE if not exists `sys_info` (
+    `id`    INTEGER PRIMARY KEY AUTOINCREMENT,
+    `TimeStamp`    TEXT,
+    `Hostname`    TEXT,
+    `KernelVersion`    TEXT,
+    `CpuNum`    TEXT,
+    `CpuUtilization`    TEXT,
+    `DiskFree`    TEXT,
+    `DiskPercent`    TEXT,
+    `DiskTotal`    TEXT,
+    `DiskUsed`    TEXT,
+    `NetBytesRecv`    TEXT,
+    `NetBytesSent`    TEXT,
+    `NetPacketsRecv`    TEXT,
+    `NetPacketsSent`    TEXT,
+    `RamAval`    TEXT,
+    `RamPercent`    TEXT,
+    `RamTotal`    TEXT
+);""")
+    conn.commit()
+    return conn
 
 def get_kernel_version():
     message = ""
@@ -102,6 +123,7 @@ def retrieve_current_system_state():
     dd['Timestamp'] = get_timestamp()
     dd['Hostname'] = get_hostname()
     dd["KernelVersion"] = get_kernel_version()
+
     # Ram Stats
     mem = PS.virtual_memory()
     dd["RamTotal"] = str(mem.total)
@@ -131,19 +153,26 @@ def retrieve_current_system_state():
     dd["DiskTotal"] = str(usage.total)
     return dd
 
+def update_database(values_dict, conn):
+    c = conn.cursor()
+    sql = "INSERT INTO sys_info "
+    c.execute()
+    pass
+
+
 def calculate_network_speed(data_queue):
-    start_time = time.time()
+    start_time = time()
     counter = PS.net_io_counters()
     total = (counter.bytes_sent, counter.bytes_recv)
     while True:
         last_total = total
-        time.sleep(1)  # Wait 1 second
-        cur_time = time.time()
+        sleep(1)  # Wait 1 second
+        cur_time = time()
         counter = PS.net_io_counters()
         total = (counter.bytes_sent, counter.bytes_recv)
         upload, download = [(now - last) / (cur_time - start_time) for now, last in zip(total, last_total)]
         data_queue.append((upload, download))
-        start_time = time.time()
+        start_time = time()
 
 def get_network_throughput(num):
     output = 0
